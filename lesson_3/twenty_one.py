7c55d38d89720c9e67c591f128b93e3395249ebb
jt755A <jot755A@gmail.com>
2025-05-01 16:55:55 -0700

    twenty_one

--- lesson_3/twenty_one.py
+++ lesson_3/twenty_one.py
@@ -4,30 +4,17 @@ import os
 import time
 
 SUITS = ('Hearts', 'Diamonds', 'Clubs', 'Spades')
-# VALUES = (2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace')
-# ROYAL_ACE_VALUES = {
-#     ('Jack', 'Queen', 'King'): 10,
-#     'Ace': [1, 11]
-# }
-# DECK = {suit: VALUES for suit in SUITS}
-
 VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10',
           'Jack', 'Queen', 'King', 'Ace']
-
 DECK = [[suit, value] for suit in SUITS
         for value in VALUES]
-
-game_deck = []
-
-
 GAME_LIMIT = 21
 DEALER_LIMIT = 17
 NUM_CARDS_INITIAL_DEAL = 2
 
+game_deck = []
 player_hand = []
 dealer_hand = []
-
-# print(DECK)
 
 def initialize_deck():
     return copy.deepcopy(DECK)
@@ -45,45 +32,34 @@ def initial_deal(player, dealer):
         deal(player)
         deal(dealer)
 
-# def join_and(lst, delimiter=', ', join_word='and'):
-#     str_lst = [str(num) for num in lst]
-    
-#     joined_nums = ''
-
-#     if len(str_lst) == 0:
-#         return joined_nums
-    
-#     if len(str_lst) == 1:
-#         joined_nums += str_lst[0]
-#         return joined_nums
-    
-#     joined_nums += delimiter.join(str_lst[:-1])
-#     return f'{joined_nums} {join_word} {str_lst[-1]}'
-
-def join_and(lst, delimiter=', ', join_word='and', hidden=''):
-    str_lst = [str(num) for num in lst]
-    
+def join_and(lst, delimiter=', ', join_word='and', hidden=False):
+   
     joined_nums = ''
     
     if not hidden:
+        str_lst = [str(num) for num in lst]
         joined_nums += delimiter.join(str_lst[:-1])
         return f'{joined_nums} {join_word} {str_lst[-1]}'
     
     elif hidden:
-        joined_nums += delimiter.join(str_lst[1:])
-        return joined_nums
+        str_lst = [str(num) for num in lst[1:]]
+
+        if len(str_lst) == 1:
+            return str_lst[0]
+                     
+        joined_nums += delimiter.join(str_lst[:])
+        return f'{joined_nums} and unknown card'
 
 
 def display_hidden_hands():
-#    dealer_values = [dealer[idx][1] for idx in range(len(dealer)) if idx > 0]
-#    dealer_values = ','.join(dealer_values)
-   prompt(f'Dealer has: {join_and(dealer_hand[1:], ', ', '')} and unknown card')
+   prompt(f'Dealer has: {join_and(dealer_hand, ', ', 'and', True)} '
+          f'and unknown card')
    prompt(f'You have: {join_and(player_hand)}')
    prompt(f'Player total is {total(player_hand)}')
 
 def reveal_hands():
-    print('\n\n')
-    prompt('The final result is:')
+    # print('\n\n')
+    # prompt('The final result is:')
     prompt(f'Dealer has: {join_and(dealer_hand)}. '
            f'Dealer total is {total(dealer_hand)}')
     prompt(f'You have: {join_and(player_hand)}. '
@@ -122,56 +98,57 @@ def player_turn():
         answer = input('Hit or stay? ')
         while answer.casefold() not in ['hit', 'stay', 'h', 's']:
             prompt("That's not a valid answer, please enter 'hit' or 'stay'")
-            answer = input('Hit or stay?')
+            answer = input('Hit or stay? ')
 
         
         if answer.casefold()[0] == 'h':
 
             deal(player_hand)
-            total(player_hand)
-            display_hidden_hands()
+            prompt(f'Player now has: {join_and(player_hand)}')
+            prompt(f'Your total is now: {total(player_hand)}')
+            # display_hidden_hands()
 
             if busted(player_hand):
                 return
                         
         else:
-            prompt("You chose to stay!")
+            prompt(f"You chose to stay at {total(player_hand)}!")
             break
-     
-    # if total(player) > LIMIT:
-    #     # end game? 
-    #     busted = True
-
 
 def dealer_turn():
     while total(dealer_hand) <= DEALER_LIMIT:
         prompt(f'The dealer hits.')
         time.sleep(2)
         deal(dealer_hand)
-        prompt(f'Dealer has: {join_and(dealer_hand[1:], ',', ',')} and unknown card')
+        # prompt(f'Dealer has: {join_and(dealer_hand[1:], ',', ',')} and unknown card')
+        prompt(f'Dealer now has: {join_and(dealer_hand, ', ', 'and', True)}')
+          #f'and unknown card')
 
         if busted(dealer_hand):
             return
+            # busted_ = True
+            # return busted_
     
     time.sleep(2)
-    prompt(f'The dealer stays')
+    prompt(f'The dealer stays at {total(dealer_hand)}')
 
 def compare_cards():
+    print('==========================================')
     reveal_hands()
+
 
     if total(player_hand) < total(dealer_hand):
         prompt('Dealer wins!')
     elif total(player_hand) > total(dealer_hand):
         prompt('Player wins!')
     else:
-        prompt("It's a tie!")
-    
+        prompt("It's a tie!")    
     
 def shuffle(deck):
     random.shuffle(deck)
 
 def play_again():
-    prompt("Would you like to play again? (y or n)") # returns Boolean
+    prompt("Would you like to play again? (y or n)")
     answer = input().strip()
     while answer.casefold() not in ['y', 'yes', 'n', 'no']:
         prompt("That's not a valid choice: please enter 'y' or 'n' ")
@@ -187,6 +164,7 @@ def play_twenty_one():
     while True:
         # outer game loop
         while True:
+            # inner game loop: for play again? question
             os.system('clear')
             game_deck.clear()
             
@@ -198,9 +176,9 @@ def play_twenty_one():
 
             initial_deal(player_hand, dealer_hand)
 
-            print(f'Player hand: {player_hand}')
-            print(f'Dealer hand: {dealer_hand}')
-            print(f'Player total is {total(player_hand)}')
+            # print(f'Player hand: {player_hand}')
+            # print(f'Dealer hand: {dealer_hand}')
+            # print(f'Player total is {total(player_hand)}')
         
             display_hidden_hands()
 
@@ -208,20 +186,20 @@ def play_twenty_one():
                 
             if busted(player_hand):
                 prompt('Player busted. Dealer wins!')
+                reveal_hands()
                 break
-
-            # os.system('clear')
-            # display_hidden_hands()
 
             dealer_turn()
 
             if busted(dealer_hand):
                 prompt('Dealer busted. Player wins!')
+                reveal_hands()
                 break
 
             compare_cards()
+            break
 
-        time.sleep(4)
+        time.sleep(3)
         if not play_again():
             break            
 
