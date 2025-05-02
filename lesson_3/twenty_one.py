d06603871847b3be8b5623079ce881e76f9203bb
jt755A <jot755A@gmail.com>
2025-05-01 17:16:18 -0700

    pylint improvements

--- lesson_3/twenty_one.py
+++ lesson_3/twenty_one.py
@@ -33,29 +33,29 @@ def initial_deal(player, dealer):
         deal(dealer)
 
 def join_and(lst, delimiter=', ', join_word='and', hidden=False):
-   
+
     joined_nums = ''
-    
+
     if not hidden:
         str_lst = [str(num) for num in lst]
         joined_nums += delimiter.join(str_lst[:-1])
         return f'{joined_nums} {join_word} {str_lst[-1]}'
-    
-    elif hidden:
+
+    if hidden:
         str_lst = [str(num) for num in lst[1:]]
 
         if len(str_lst) == 1:
             return str_lst[0]
-                     
+
         joined_nums += delimiter.join(str_lst[:])
         return f'{joined_nums} and unknown card'
 
 
 def display_hidden_hands():
-   prompt(f'Dealer has: {join_and(dealer_hand, ', ', 'and', True)} '
+    prompt(f'Dealer has: {join_and(dealer_hand, ', ', 'and', True)} '
           f'and unknown card')
-   prompt(f'You have: {join_and(player_hand)}')
-   prompt(f'Player total is {total(player_hand)}')
+    prompt(f'You have: {join_and(player_hand)}')
+    prompt(f'Player total is {total(player_hand)}')
 
 def reveal_hands():
     # print('\n\n')
@@ -64,11 +64,11 @@ def reveal_hands():
            f'Dealer total is {total(dealer_hand)}')
     prompt(f'You have: {join_and(player_hand)}. '
            f'Player total is {total(player_hand)}')
-    
+
 def total(cards):
-    
-    values = [card for card in cards]
-    
+
+    values = list(cards)
+
     sum_values = 0
 
     for value in values:
@@ -86,21 +86,21 @@ def total(cards):
         sum_values -= 10
         aces_count -= 1
 
-    return sum_values    
+    return sum_values
 
 
 def busted(hand):
     return total(hand) > GAME_LIMIT
 
 def player_turn():
-    
+
     while True:
         answer = input('Hit or stay? ')
         while answer.casefold() not in ['hit', 'stay', 'h', 's']:
             prompt("That's not a valid answer, please enter 'hit' or 'stay'")
             answer = input('Hit or stay? ')
 
-        
+
         if answer.casefold()[0] == 'h':
 
             deal(player_hand)
@@ -110,14 +110,14 @@ def player_turn():
 
             if busted(player_hand):
                 return
-                        
+
         else:
             prompt(f"You chose to stay at {total(player_hand)}!")
             break
 
 def dealer_turn():
     while total(dealer_hand) <= DEALER_LIMIT:
-        prompt(f'The dealer hits.')
+        prompt('The dealer hits.')
         time.sleep(2)
         deal(dealer_hand)
         # prompt(f'Dealer has: {join_and(dealer_hand[1:], ',', ',')} and unknown card')
@@ -128,7 +128,7 @@ def dealer_turn():
             return
             # busted_ = True
             # return busted_
-    
+
     time.sleep(2)
     prompt(f'The dealer stays at {total(dealer_hand)}')
 
@@ -142,8 +142,8 @@ def compare_cards():
     elif total(player_hand) > total(dealer_hand):
         prompt('Player wins!')
     else:
-        prompt("It's a tie!")    
-    
+        prompt("It's a tie!")
+
 def shuffle(deck):
     random.shuffle(deck)
 
@@ -155,19 +155,19 @@ def play_again():
         answer = input()
 
     if answer[0].casefold() != 'y':
-        return False   
-    
+        return False
+
     return True
 
 def play_twenty_one():
-        
+
     while True:
         # outer game loop
         while True:
             # inner game loop: for play again? question
             os.system('clear')
             game_deck.clear()
-            
+
             player_hand.clear()
             dealer_hand.clear()
 
@@ -179,11 +179,11 @@ def play_twenty_one():
             # print(f'Player hand: {player_hand}')
             # print(f'Dealer hand: {dealer_hand}')
             # print(f'Player total is {total(player_hand)}')
-        
+
             display_hidden_hands()
 
             player_turn()
-                
+
             if busted(player_hand):
                 prompt('Player busted. Dealer wins!')
                 reveal_hands()
@@ -201,7 +201,7 @@ def play_twenty_one():
 
         time.sleep(3)
         if not play_again():
-            break            
+            break
 
     prompt('Thank you for playing twenty-one!')
 
